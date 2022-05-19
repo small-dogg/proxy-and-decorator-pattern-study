ce28b0e1b2638d944139ad038d85a0c5a5f0ceb6
small-dogg <sy920112@naver.com>
2022-05-19 17:00:29 +0900

    feat: 인터페이스 기반 프록시와 구체 클래스 기반 프록시

---
+++ src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteClientTest.java
@@ -0,0 +1,27 @@
+package hello.proxy.pureproxy.concreateproxy;
+
+import hello.proxy.pureproxy.concreateproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreateproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreateproxy.code.TimeProxy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConcreteClientTest {
+
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient concreteClient = new ConcreteClient(timeProxy);
+        concreteClient.execute();
+
+    }
+}
